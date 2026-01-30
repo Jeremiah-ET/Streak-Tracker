01e34def1a5a859554aec957bc74f42969b5f6d8
Jeremiah-ET <techmartbusinessservice@gmail.com>
2026-01-30 16:18:24 -0500

    Sidebar css but needs update

--- public/index.js
+++ public/index.js
@@ -1,4 +1,17 @@
 const streakButton = document.getElementById("streakButton")
+const mainContainer = document.getElementById("main")
+const streakText = document.getElementById("streakText")
+const StreakMinusButton = document.getElementById("removeStreak")
+const resetStreak = document.getElementById("resetStreak")
+
+let streakNumber = 0;
 
 streakButton.addEventListener("click", (e) => {
+  console.log(`Streak Added: ${streakNumber}`)
+  streakNumber += 1;
+  streakText.textContent = `Your current Streak is: ${streakNumber}`
+  mainContainer.appendChild(streakDeclaration);
+  // need to add limit on how many times when pressed
+  // if (streakDeclaration.value = !0) {}
+  
 })
