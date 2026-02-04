6735a8de40cd6c9daf8654c07d5736ca3daf300e
Jeremiah-ET <techmartbusinessservice@gmail.com>
2026-02-04 07:53:48 -0500

    Make correct port on 8000

--- express.js
+++ express.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, ()=> {
   console.log(`Connected to server on ${port}`)
+})
+
+app.get("/", ()=> {
+  console.log("Connect to Homepage")
 })
