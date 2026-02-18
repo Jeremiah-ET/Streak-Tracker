b3fb40da3566d5556258747198b130d801e5a7c9
Jeremiah-ET <techmartbusinessservice@gmail.com>
2026-02-17 20:11:33 -0500

    Changes to modules which are better but need to learn more of the syntax and format

--- express.js
+++ express.js
@@ -1,17 +1,25 @@
-const express = require("express");
-const path = require("path");
-require("dotenv").config();
+import express from "express";
+import dotenv from "dotenv";
+import OpenAI from "openai";
+import { fileURLToPath } from "url";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, "public");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
-  console.log(`Connected to server on ${port}`)
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
-app.get("/", ()=> {
-  console.log("Connect to Homepage")
-})
+app.listen(port, () => {
+  console.log(`Connected to server on ${port}`);
+});
